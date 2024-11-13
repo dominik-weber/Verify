430ae7ed5137c138c3df51ab60774b123ff9f891
Simon Cropp <simon.cropp@gmail.com>
2024-11-14 07:37:48 +1100

    .

--- src/Benchmarks/ProjectDirectoryFinderBenchmarks.cs
+++ src/Benchmarks/ProjectDirectoryFinderBenchmarks.cs
@@ -8,7 +8,14 @@ public class ProjectDirectoryFinderBenchmarks
         var solutionDirectory = SolutionDirectoryFinder.Find(Environment.CurrentDirectory);
         directory = Path.Combine(solutionDirectory, @"Verify.Tests\VerifyDirectoryTests.WithDirectory\nested.with.dot");
     }
-
+    public int BirthYear {
+        get => field;
+        set {
+            if (value < 0) // Validation code
+                throw new ArgumentOutOfRangeException("Age cannot be negative.");
+            field = value;
+        }
+    }
     [Benchmark]
     public string FindProjectDirectory() =>
         ProjectDirectoryFinder.Find(directory);
